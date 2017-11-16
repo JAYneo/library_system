4ead45a623dbf8f1f081944bd2531d7543c36a5c
Jesse Y <jesse@arrayschool.com>
2017-11-16 14:25:20 -0700

    update

--- scripts/min/main-min.js
+++ scripts/min/main-min.js
@@ -1,5 +1,7 @@
- var bookTemplate = $('#templates .book')
- var bookTable = $('#bookTable')
+var bookTemplate = $('#templates .book')
+var borrowerTemplate = $('#templates .borrower')
+var bookTable = $('#bookTable')
+var borrowerTable = $('#borrowerTable')
 
 
 //library id 131
@@ -17,6 +19,14 @@ function addBookToPage(bookData) {
   bookTable.append(book)
 }
 
+function addBorrowerToPage(borrowerData){
+  var borrower = borrowerTemplate.clone()
+  borrower.attr('data-id', borrowerData.id)
+  borrower.find('.borrowerFirstName').text(borrowerData.firstname)
+  borrower.find('.borrowerLastName').text(borrowerData.lastname)
+  borrowerTable.append(borrower)
+}
+
 var getBooksRequest = $.ajax({
   type: 'GET',
   url: `${baseURL}/books`,
@@ -25,6 +35,39 @@ var getBooksRequest = $.ajax({
 getBooksRequest.done( (dataFromServer) => {
   dataFromServer.forEach((bookData) => {
     addBookToPage(bookData)
+  })
+})
+
+var getBorrowerRequest = $.ajax({
+  type: 'GET',
+  url: `${baseURL}/borrowers`,
+})
+
+getBorrowerRequest.done(( dataFromServer) => {
+  dataFromServer.forEach((borrowerData) => {
+    addBorrowerToPage(borrowerData)
+  })
+})
+
+$('#createBookButton').on('click', () => {
+  var bookData = {}
+
+  bookData.title = $('.addBookTitle').val()
+  bookData.description = $('.addBookDescription').val()
+  bookData.image_url = $('.addBookImageURL').val()
+
+  var createBookRequest = $.ajax({
+    type:'POST',
+    url: baseURL + '/books',
+    data: {
+      book: bookData
+    }
+  })
+
+  createBookRequest.done((dataFromServer) => {
+    addBookToPage(dataFromServer)
+    $('#addBookModal').modal('hide')
+    $('#addBookForm')[0].reset()
   })
 })
 
